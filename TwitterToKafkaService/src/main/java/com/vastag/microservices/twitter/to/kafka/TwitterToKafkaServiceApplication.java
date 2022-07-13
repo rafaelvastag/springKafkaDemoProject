866a18f8f047a500e5e749a670ac9825c2bb081c
Rafael Vastag <rgvastag@gmail.com>
2022-07-13 15:54:15 -0300

    kafka streaming config properties

--- TwitterToKafkaService/src/main/java/com/vastag/microservices/twitter/to/kafka/TwitterToKafkaServiceApplication.java
+++ TwitterToKafkaService/src/main/java/com/vastag/microservices/twitter/to/kafka/TwitterToKafkaServiceApplication.java
@@ -1,13 +1,33 @@
 package com.vastag.microservices.twitter.to.kafka;
 
+import java.util.Arrays;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class TwitterToKafkaServiceApplication {
+import com.vastag.microservices.twitter.to.kafka.config.TwitterToKafkaServiceConfigData;
 
-	public static void main(String ...args) {
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@SpringBootApplication
+public class TwitterToKafkaServiceApplication implements CommandLineRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
+
+	private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+	public static void main(String... args) {
 		SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		LOG.info("App starts...");
+		LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
 	}
 
 }
---
+++ TwitterToKafkaService/src/main/java/com/vastag/microservices/twitter/to/kafka/config/TwitterToKafkaServiceConfigData.java
@@ -0,0 +1,15 @@
+package com.vastag.microservices.twitter.to.kafka.config;
+
+import java.util.List;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+import lombok.Data;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "twitter-to-kafka-service")
+public class TwitterToKafkaServiceConfigData {
+	private List<String> twitterKeywords;
+}
