62ed9914690fbb64f54469a143871de0567cc9e6
Rafael Vastag <rgvastag@gmail.com>
2022-07-13 21:51:15 -0300

    twitter features

--- TwitterToKafkaService/java/com/vastag/microservices/twitter/to/kafka/TwitterToKafkaServiceApplication.java
+++ TwitterToKafkaService/java/com/vastag/microservices/twitter/to/kafka/TwitterToKafkaServiceApplication.java
@@ -4,14 +4,18 @@ import java.util.Arrays;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 import com.vastag.microservices.twitter.to.kafka.config.TwitterToKafkaServiceConfigData;
+import com.vastag.microservices.twitter.to.kafka.runner.StreamRunner;
 
 import lombok.RequiredArgsConstructor;
 
+@ComponentScan(basePackages = "com.vastag.microservices.twitter.to.kafka")
 @RequiredArgsConstructor
 @SpringBootApplication
 public class TwitterToKafkaServiceApplication implements CommandLineRunner {
@@ -19,6 +23,9 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 	private static final Logger LOG = LoggerFactory.getLogger(TwitterToKafkaServiceApplication.class);
 
 	private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+	@Qualifier(value = "twitterKafkaStreamRunner")
+	private final StreamRunner streamRunner;
 
 	public static void main(String... args) {
 		SpringApplication.run(TwitterToKafkaServiceApplication.class, args);
@@ -28,6 +35,6 @@ public class TwitterToKafkaServiceApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		LOG.info("App starts...");
 		LOG.info(Arrays.toString(twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[] {})));
+		streamRunner.start();
 	}
-
 }
---
+++ TwitterToKafkaService/java/com/vastag/microservices/twitter/to/kafka/runner/impl/TwitterKafkaStreamRunner.java
@@ -0,0 +1,58 @@
+package com.vastag.microservices.twitter.to.kafka.runner.impl;
+
+import java.util.Arrays;
+
+import javax.annotation.PreDestroy;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import com.vastag.microservices.twitter.to.kafka.config.TwitterToKafkaServiceConfigData;
+import com.vastag.microservices.twitter.to.kafka.listener.TwitterKafkaStatusListener;
+import com.vastag.microservices.twitter.to.kafka.runner.StreamRunner;
+
+import twitter4j.FilterQuery;
+import twitter4j.TwitterException;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+
+@Component(value = "twitterKafkaStreamRunner")
+public class TwitterKafkaStreamRunner implements StreamRunner {
+
+	private static final Logger LOG = LoggerFactory.getLogger(TwitterKafkaStreamRunner.class);
+
+	private final TwitterToKafkaServiceConfigData twitterToKafkaServiceConfigData;
+
+	private final TwitterKafkaStatusListener twitterKafkaStatusListener;
+
+	private TwitterStream twitterStream;
+
+	public TwitterKafkaStreamRunner(TwitterToKafkaServiceConfigData configData,
+			TwitterKafkaStatusListener statusListener) {
+		this.twitterToKafkaServiceConfigData = configData;
+		this.twitterKafkaStatusListener = statusListener;
+	}
+
+	@Override
+	public void start() throws TwitterException {
+		twitterStream = new TwitterStreamFactory().getInstance();
+		twitterStream.addListener(twitterKafkaStatusListener);
+		addFilter();
+	}
+
+	@PreDestroy
+	public void shutdown() {
+		if (twitterStream != null) {
+			LOG.info("Closing twitter stream!");
+			twitterStream.shutdown();
+		}
+	}
+
+	private void addFilter() {
+		String[] keywords = twitterToKafkaServiceConfigData.getTwitterKeywords().toArray(new String[0]);
+		FilterQuery filterQuery = new FilterQuery(keywords);
+		twitterStream.filter(filterQuery);
+		LOG.info("Started filtering twitter stream for keywords {}", Arrays.toString(keywords));
+	}
+}
